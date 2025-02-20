01e80417461e5ceb9ebb617308fe5c4a7e9e7100
gduarte2077 <gduarte2077@gmail.com>
2025-02-20 18:54:29 -0300

    corrige el comportamiento del ScrollReveal

--- asssets/js/main.js
+++ asssets/js/main.js
@@ -77,10 +77,11 @@ const sr = ScrollReveal({
   distance: "60px",
   duration: 3000,
   delay: 400,
-  //reset: true
+  reset: false,
+  once: true
 });
 
-sr.reveal(`.home__data, .explore__data, explore__user, .footer__container`);
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`);
 sr.reveal(`.home__card`, { delay: 600, distance: "100px", interval: 100 });
 sr.reveal(`.about__data, .join__image`, { origin: "right" });
 sr.reveal(`.about__image, .join__data`, { origin: "left" });
